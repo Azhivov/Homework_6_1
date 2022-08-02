9658575b7ff8855fb2736b0ecc70de4f26260215
azhiv <a.zhivov@gmail.com>
2022-08-02 14:32:49 +0300

    first commit HW_3

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,22 @@ public class Main {
 
 
         int sum = 0;
-        for (int i = 0; i < myArray.length; i++) {
-            sum += myArray[i];
+        for (int spendingDay : myArray) {
+            sum += spendingDay;
         }
         System.out.printf("Сумма трат за месяц %s рублей", sum);
+        System.out.println();
+
+        int min = myArray[0];
+        int max = myArray[0];
+        for (int i = 1; i < myArray.length; i++) {
+            if (min < myArray[i]) {
+                min = myArray[i];
+            } else if (max < myArray[i]) ;
+            max = myArray[i];
+        }
+        System.out.printf("Минимальная сумма за день составила %s рубдей. " +
+                "Ммаксимальная сумма за день составила %s рубдей. ", min, max);
     }
 
         public static int[] generateRandomArray() {
